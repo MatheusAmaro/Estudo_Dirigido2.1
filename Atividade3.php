b922ff1483cba1541c46e7cbe4e3b25bdd3efda0
Matheus Amaro Maciel <48133433+MatheusAmaro@users.noreply.github.com>
2019-04-19 08:48:38 -0300

    Create Atividade3.php

---
+++ Atividade3.php
@@ -0,0 +1,21 @@
+<?php
+
+print "Diga o gênero que vc pertence : \n\n";
+
+print "Digite F para feminino e M para masculino \n\n" ;
+
+$genero = (string) fgets (STDIN)
+     
+     if ($genero == F ){
+        print "Feminino \n\n";
+    }
+
+    if ($genero == M ){
+        print "Masculino \n\n";
+    }
+
+    if ($genero= A or B){
+        print "Parace que você digitou algo errado : \n\n";
+    }
+    
+    print "Fim Algoritmo" ;
