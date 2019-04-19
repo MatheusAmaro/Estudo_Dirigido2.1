4c97dffd4fd4032a245030f2df6e56a1d89b12df
Matheus Amaro Maciel <48133433+MatheusAmaro@users.noreply.github.com>
2019-04-19 08:20:50 -0300

    Create Atividade1.php

---
+++ Atividade1.php
@@ -0,0 +1,18 @@
+<?php
+
+print "Digite dois números e diga qual é o maior deles :";
+
+$num1: (int) fgets (STDIN);
+$num2: (int) fgets (STDIN);
+
+print "Agora diga o mair dos dois números :" ;
+
+if($num1 > $num2){
+        print "O maior valor dos dois é $num1 \n\n"
+        }
+
+    if ($num2 > $num1){
+        print "O maior valor dos dois é $num2 \n\n";
+    }
+    
+    print "Fim Algoritmo" ;
