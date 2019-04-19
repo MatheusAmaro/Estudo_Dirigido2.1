dc9149ec1e2e0f4373fac87931f43fcaba96ec05
Matheus Amaro Maciel <48133433+MatheusAmaro@users.noreply.github.com>
2019-04-19 09:27:12 -0300

    Create Atividade7.php

---
+++ Atividade7.php
@@ -0,0 +1,11 @@
+<?php 
+
+print " Digite três números e diga é o maior e o menor entre eles :" ;
+
+$n1 : (int) fgets (STDIN);
+$n2 : (int) fgets (STDIN);
+$n3 : (int) fgets (STDIN);
+
+print "Agora diga o maioir entre os três :" ;
+ 
+if ()
