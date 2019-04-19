c62d23c3c1b1cecef4dd1fdd61c55c0edcaa4798
Matheus Amaro Maciel <48133433+MatheusAmaro@users.noreply.github.com>
2019-04-19 09:06:15 -0300

    Create Atividade5.php

---
+++ Atividade5.php
@@ -0,0 +1,26 @@
+<?php 
+ print " Digite duas notas pertensentes a uma aluno, e depois some-as e diga a média do aluno :" ;
+ 
+ 
+ $nota1 = (int)  fgets (STDIN);
+ 
+ $nota2 = (int) fgets  (STDIN);
+ 
+ print "Agora calcule a média" ; 
+ 
+ $nota1 + $ $nota2 = $media ;
+ 
+ if ($media >= 7) {
+    print " O aluno está aprovado por média" ;
+ }
+ 
+ if ($media < 7 ) {
+    print "O aluno está reprovado" ;
+ }
+ 
+ if ($media = 10 ){
+ print "O aluno está aprovado com distinção" ;
+ }
+ 
+ 
+ PRINT "Fim Algoritmo" ;
