59f8e05dc3e9f30ad53177d7f78643c824513d27
Matheus Amaro Maciel <48133433+MatheusAmaro@users.noreply.github.com>
2019-04-19 08:33:46 -0300

    Create Atividade2.php

---
+++ Atividade2.php
@@ -0,0 +1,21 @@
+<?php 
+
+print "Digite um número e diga se ele é positivo ou negativo :" ;
+
+$num: (int) fgets (STDIN);
+
+ if ($num> 0){
+        print "O $num é positivo \n\n";
+    }
+
+    if ($num < 0){
+        print "O $num é negativo \n\n";
+    }
+
+    if ($num == 0){
+        print "O $num\n\n";
+    }
+
+
+     print "Fim Algoritmo";
+ 
