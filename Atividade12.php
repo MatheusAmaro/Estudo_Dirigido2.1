fdf01a12d9e6df188b0e45c15fe877320fa79eea
Matheus Amaro Maciel <48133433+MatheusAmaro@users.noreply.github.com>
2019-04-22 20:51:42 -0300

    Create Atividade12.php

---
+++ Atividade12.php
@@ -0,0 +1,12 @@
+<?php
+
+print " Mostreme os números de 1 a 100 :" ;
+
+$contador = 0 ;
+while ( $contador > 1 ) {
+ print= " $contador ";
+ $contador= $contador+1
+}
+
+
+print "Fim Do Algoritmo ";
