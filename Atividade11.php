757f1e0564ee7a65ff08d1f459657b41428915dc
Matheus Amaro Maciel <48133433+MatheusAmaro@users.noreply.github.com>
2019-04-22 20:39:21 -0300

    Update Atividade11.php

--- Atividade11.php
+++ Atividade11.php
@@ -1,3 +1,28 @@
 <?php
 
-print " " ;
+print " Digite um número menor 1000 : /n/n " ;
+
+$n = input ("digite um número menor que 1000 ");
+$nStr = str ($n);
+$qtnum = len ($nStr);
+
+if ($qtnum == 3) {
+    centena = $nStr[0:1]
+    dezena = $nStr[1:2]
+    unidade = $nStr[2:3]
+    print ($nStr+" = "+centena+" centenas , "+dezena+" dezenas, "+unidade+ " unidades");
+}
+
+if ($qtnum== 2) {
+    dezena = $numeroStr[0:1]
+    unidade = $numeroStr[1:2]
+    print ($nStr+" = "+dezena+" dezenas, "+unidade+ " unidades") ;
+}
+
+if ($qtnum == 1 :) {
+    unidade = $nStr[0:1]
+    print ($nStr+" = "+unidade+ " unidades");
+}
+
+
+print "Fim do Algoritmo" ;
