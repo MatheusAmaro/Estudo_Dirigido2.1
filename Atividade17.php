59aaf0608b519f5074abdc427b3fd27c1e59b530
Matheus Amaro Maciel <48133433+MatheusAmaro@users.noreply.github.com>
2019-04-23 13:08:29 -0300

    Update Atividade17.php

--- Atividade17.php
+++ Atividade17.php
@@ -1,1 +1,20 @@
 <?php 
+
+print " Digite dois números para serem a base :/n/n " ;
+
+$base1= (int) fgets (STDIN);
+$base2= (int) fgets (STDIN);
+
+print " Agora digite mais dois números para serem os expoentes das bases acima : /n/n ";
+
+$exp1= (int) fgets (STDIN) ;
+$exp2= (int) fgets (STDIN) ;
+
+print " Agora calcule o primeiro número utilizando o segundo como expoente : "  ;
+
+
+
+
+
+
+print " Fim do Algoritmo " ;
