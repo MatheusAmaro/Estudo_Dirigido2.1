e632f91ae38ec454bbcc267577d05694bb0a28a0
Matheus Amaro Maciel <48133433+MatheusAmaro@users.noreply.github.com>
2019-04-23 20:19:35 -0300

    Update Atividade6.php

--- Atividade6.php
+++ Atividade6.php
@@ -8,14 +8,33 @@ $n3 = (int) fgets (STDIN);
 
 print "Agora qual é o maior deles : \n\n" ;
 
-if ($n1 > $n2 or $n1 > $n3 ) {
+# Achando o maior número #
+
+if ($n1 > $n2  ) {
     print "O $n1 é o maior número dos três ." ; 
 }
 
-if($n2 > $n1 or $n2 > $n3){
-print "então $n2 é o maior número dos três ." ; }
-
-
-elseif ($n3 > $n1 or $n3 > $n2){
-    print " Logo  $n3 é o maior número dos três .";
+if ($n1 > $n2) {
+    print " O $n1 é o maior número dos três ." ; 
 }
+
+if ($n2 > $n1) {
+     print " O $n2 é o maior número dos três ." ;
+}
+
+if ($n2 > $n3) {
+     print "O $n2 é o maior número dos três .";
+}
+
+if ($n3 > $n1 ) {
+    print " O $n3 é o maior número dos três . ";
+}
+
+if ($n3 > $n2) {
+    print " O $n3 é o maior número dos três . ";
+}
+
+
+print " Fim do Algoritmo " ;
+
+
