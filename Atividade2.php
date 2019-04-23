a7434fb0256d0f5ee3ad2bf6df2e2f02944cb3f9
Matheus Amaro Maciel <48133433+MatheusAmaro@users.noreply.github.com>
2019-04-23 14:12:22 -0300

    Update Atividade2.php

--- Atividade2.php
+++ Atividade2.php
@@ -1,20 +1,23 @@
 <?php 
 
-print "Digite um número e diga se ele é positivo ou negativo :" ;
 
-$num: (int) fgets (STDIN);
+print "Digite um número e diga se ele é positivo ou negativo :\n\n" ;
+
+$num = (int) fgets (STDIN);
+
 
  if ($num> 0){
-        print "O $num é positivo \n\n";
+        print "O  $num  é  um  número positivo \n\n";
     }
 
     if ($num < 0){
-        print "O $num é negativo \n\n";
+        print "O  $num  é  um  número negativo \n\n";
     }
 
     if ($num == 0){
-        print "O $num\n\n";
+        print "O  $num  é  um  número neutro \n\n" ;
     }
+
 
 
      print "Fim Algoritmo";
