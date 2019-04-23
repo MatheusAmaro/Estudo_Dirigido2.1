86d59f95fe178708bcc2cc701340c7eca7f27c3d
Matheus Amaro Maciel <48133433+MatheusAmaro@users.noreply.github.com>
2019-04-23 13:26:16 -0300

    Update Atividade20.php

--- Atividade20.php
+++ Atividade20.php
@@ -1,9 +1,48 @@
 <?php
 
-print "" ; 
+print "Monte três figuras geometricas com (*) : " ; 
 
 
+#figura 1 #
+$contador == * ;
 
+wile($contador <= 8 ) {
+ print $contador ; $contador= $contador + 1;
+}
+wile($contador <= 8 ) {
+ print $contador ; $contador= $contador + 1;
+}
+wile($contador <= 8 ) {
+ print $contador ; $contador= $contador + 1;
+}
+
+------------------------------------------------------
+#figura 2#
+$contador == * ;
+
+while ($contador = 1 ) {
+ print $contador; $contador = $contador + 1;
+}
+while ($contador = 1 ) {
+ print $contador; $contador = $contador + 1;
+}
+while ($contador = 1 ) {
+ print $contador; $contador = $contador + 1;
+}
+
+-------------------------------------------------------
+#figura 3 #
+$contador == * ;
+
+while ($contador = 1 ) {
+ print $contador; $contador = $contador + 1;
+}
+while ($contador = 1 ) {
+ print $contador; $contador = $contador + 1;
+}
+while ($contador = 1 ) {
+ print $contador; $contador = $contador + 1;
+}
 
 
 
