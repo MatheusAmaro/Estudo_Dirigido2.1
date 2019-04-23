b8aa21ede76fcb1e7867900dce7b94e861726838
Matheus Amaro Maciel <48133433+MatheusAmaro@users.noreply.github.com>
2019-04-22 21:53:00 -0300

    Update Atividade14.php

--- Atividade14.php
+++ Atividade14.php
@@ -1,1 +1,14 @@
 <?php
+
+print " Mostre-me os números de 100 a 1 : /n/n" ; 
+
+$contador = 0 ;
+
+while ( $contador <100 ) {
+
+ print= " $contador ";
+
+ $contador= $contador -1 ;
+}
+
+print " Fim do Algoritmo" ; 
