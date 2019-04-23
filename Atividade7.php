37489c2e89b88452dd511be94c3c18e68824b77f
Matheus Amaro Maciel <48133433+MatheusAmaro@users.noreply.github.com>
2019-04-23 15:09:59 -0300

    Update Atividade7.php

--- Atividade7.php
+++ Atividade7.php
@@ -1,26 +1,26 @@
 <?php 
+print " Digite três números e diga é o maior e o menor entre eles :\n\n" ;
 
-print " Digite três números e diga é o maior e o menor entre eles :" ;
-
-$n1 : (int) fgets (STDIN);
-$n2 : (int) fgets (STDIN);
-$n3 : (int) fgets (STDIN);
+$n1 = (int) fgets (STDIN);
+$n2 = (int) fgets (STDIN);
+$n3 = (int) fgets (STDIN);
 
 print "Agora diga o maioir entre os três :" ;
  
-if ($n1 > $n2 or $n3) {
+if ($n1 > $n2 or $n1 > $n3) {
     print "O $n1 é o maior número dos três :" ; 
 }
-
-if ($n2 > $n1 or $n3) {
+if ($n2 > $n1 or $n2 > $n3) {
     print "O $n2 é o maior número dos três :" ; 
 }
-
-if ($n3 > $n1 or $n2) {
+if ($n3 > $n1 or $n3 > $n2) {
     print "O $n3 é o maior número dos três :" ; 
 }
 
-print "Agora diga qual o menor dos três :" ;
+
+
+print "Agora diga qual o menor dos três :\n\n" ;
+
 
     if ($a < $b and $a < $c){
         print "O $n1 é o menor número dos três  \n\n";
