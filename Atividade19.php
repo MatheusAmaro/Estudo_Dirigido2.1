3940a18763d3efc0e9c0428bdc51d5a84dfb765a
Matheus Amaro Maciel <48133433+MatheusAmaro@users.noreply.github.com>
2019-04-22 21:48:06 -0300

    Update Atividade19.php

--- Atividade19.php
+++ Atividade19.php
@@ -1,1 +1,13 @@
 <?php
+
+ print " Gera a serie Fibonacci até que a sequência seja maior que 500 /n/n : "
+   
+$Fibo = [1,1]
+$i = 0
+   
+$num = (int) fgets (STDIN) ;
+
+while $num > len($Fibo):
+	$Fibo.append($Fibo[$i] + $Fibo[$i+1])
+	$i + = 1
+
