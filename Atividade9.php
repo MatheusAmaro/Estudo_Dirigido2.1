41325536ad8cdbe50f0e4a4958796ca3b8bccdd0
Matheus Amaro Maciel <48133433+MatheusAmaro@users.noreply.github.com>
2019-04-23 20:44:32 -0300

    Update Atividade9.php

--- Atividade9.php
+++ Atividade9.php
@@ -8,18 +8,34 @@ $n3= (int) fgets (STDIN) . "\n\n" ;
 
 print "Agora leia esses números de maneira decrescente : \n\n" ;
 
-     if ($n1 > $n2 and $n1 > $n3 and $n2 > $n3){
+     if ($n1 > $n2  > $n3 ){
         print " Então a ordem é  $n1 , $n2, $n3 \n\n";
     }
     
-     if ($n2 > $n1 and $n2> $n3 and $n1 > $n3){
+     if ($n2 >  $n1> $n3 ){
         print " Então a ordem é  $n2 , $n1, $n3 \n\n";
     }
     
-     if ($n3 > $n1 and $n3 > $n2 and $n2 > $n1){
-        print " Então a ordem é  $n3 , $n2, $n1 \n\n";
+     if ($n3 > $n1 > $n2 ){
+        print " Então a ordem é  $n3 , $n1 , $n2 \n\n";
+    }
+
+    if ($n3 > $n2 > $n1 ){
+        print " Então a ordem é  $n3 , $n2 , $n1 \n\n";
+    }
+
+     if ($n2 > $n3 > $n1 ){
+        print " Então a ordem é  $n2 , $n3 , $n1 \n\n";
+    }
+
+    if ($n1 = $n2 = $n3 ){
+        print " A ordem está invalida segundo o que foi pedido \n\n";
     }
   
+  
+
+
+
     print "Fim Algoritmo" ;
     
     
