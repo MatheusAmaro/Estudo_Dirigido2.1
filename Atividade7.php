8945015eb30b13b083ab8f55748998895db008b8
Matheus Amaro Maciel <48133433+MatheusAmaro@users.noreply.github.com>
2019-04-23 20:26:42 -0300

    Update Atividade7.php

--- Atividade7.php
+++ Atividade7.php
@@ -5,33 +5,50 @@ $n1 = (int) fgets (STDIN);
 $n2 = (int) fgets (STDIN);
 $n3 = (int) fgets (STDIN);
 
-print "Agora diga o maioir entre os três :" ;
+print "Agora diga o maior número entre os três :" ;
  
-if ($n1 > $n2 or $n1 > $n3) {
+if ($n1 > $n2 ) {
     print "O $n1 é o maior número dos três :" ; 
 }
-if ($n2 > $n1 or $n2 > $n3) {
+if ($n1 > $n3 ) {
+    print "O $n1 é o maior número dos três :" ; 
+}
+if ($n2 > $n1 ) {
     print "O $n2 é o maior número dos três :" ; 
 }
-if ($n3 > $n1 or $n3 > $n2) {
+if ($n2 > $n3 ) {
+    print "O $n2 é o maior número dos três :" ; 
+} 
+if ($n3 > $n1 ) {
     print "O $n3 é o maior número dos três :" ; 
 }
-
-
-
+if ($n3 > $n2 ) {
+    print "O $n3 é o maior número dos três :" ; 
+ 
+---------------------------------------------------- 
+ 
+ 
 print "Agora diga qual o menor dos três :\n\n" ;
 
+ 
+if ($n1 < $n2 ) {
+    print "O $n1 é o menor número dos três :" ; 
+}
+if ($n1 < $n3 ) {
+    print "O $n1 é o menor número dos três :" ; 
+}
+if ($n2 < $n1 ) {
+    print "O $n2 é o menor número dos três :" ; 
+}
+if ($n2 < $n3 ) {
+    print "O $n2 é o menor número dos três :" ; 
+} 
+if ($n3 < $n1 ) {
+    print "O $n3 é o menor número dos três :" ; 
+}
+if ($n3 < $n2 ) {
+    print "O $n3 é o menor número dos três :" ; 
 
-    if ($a < $b and $a < $c){
-        print "O $n1 é o menor número dos três  \n\n";
-    }
-
-    if ($n2 < $n1 and $n2 < $n3){
-        print "O $n2 é o menor número dos três  \n\n" ; 
-    }
-
-    if ($n3 < $n1 and $n3 < $n2) {
-        print "O $n3 é o menor número dos três \n\n" ;   
-    }
-
+ 
+ 
 print "Fim do Algoritmo" ; 
