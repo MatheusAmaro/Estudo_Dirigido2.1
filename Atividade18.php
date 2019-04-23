77ceb771ad7962ffb20df3910a18a2745f5c7b01
Matheus Amaro Maciel <48133433+MatheusAmaro@users.noreply.github.com>
2019-04-22 21:41:31 -0300

    Update Atividade18.php

--- Atividade18.php
+++ Atividade18.php
@@ -1,1 +1,40 @@
 <?php
+
+print " Digite dez números inteiros : /n/n " ;
+
+$num = (int) fgets (STDIN) ;
+
+print " Agora diga quais deles são números pares e quais são números ipares , depois calcule a quantidade de números pares e imapres encontrados " ;
+
+ if ( $n %2 ==0 {
+  print "Esse número é par :"
+ }
+     else {
+     print " Esse número é impar : " ;
+     }
+# Contando quantos números impares apareceram #    
+ if ($num & 1) {
+   if ($contador_impar)
+       $menor_impar = numero;
+   else {
+       if ($num < $menor_impar)
+           $menor_impar = $num;
+   }
+   $contador_impar++;
+ # Contando quantos números pares apareceram#
+   
+   if ($num & 2 ) {
+     if ($contador_par)
+       $menor_par = $num;
+   }
+   $contador_par++;
+    
+   print " Fim do Algoritmo " ;
+   
+   
+   
+   
+   
+   
+ print " Fim Do Algoritmo ";
+     
