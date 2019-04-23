372795db993b6fff7d1f3249eb9ec3494bc7992c
Matheus Amaro Maciel <48133433+MatheusAmaro@users.noreply.github.com>
2019-04-22 22:22:06 -0300

    Update Atividade14.php

--- Atividade14.php
+++ Atividade14.php
@@ -1,14 +1,40 @@
 <?php
 
-print " Mostre-me os números de 100 a 1 : /n/n" ; 
+print " Mostre-me os números de 1 há 50 e mostre somente os números impares : /n/n" ; 
 
-$contador = 0 ;
+$num = 1 ;
 
-while ( $contador <100 ) {
+while ( $num < 50  ) {
+
+ print= " $num ";
+
+ $num= $num + 1  ;
+}
+ if ($contador % 2 == 0){
+     echo "par";
+} else {
+     echo "impar";
+}
+
+
+
+
+
+print " Fim do Algoritmo ";
+
+#$contador = 0 ;
+
+while ( $contador > 1  ) {
 
  print= " $contador ";
 
  $contador= $contador -1 ;
-}
+}#
+
+
+
+
+
+
 
 print " Fim do Algoritmo" ; 
