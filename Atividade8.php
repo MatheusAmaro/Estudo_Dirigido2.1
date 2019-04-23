d3c62bff0129df96c094aaf278f1c9c0341eed17
Matheus Amaro Maciel <48133433+MatheusAmaro@users.noreply.github.com>
2019-04-23 20:31:58 -0300

    Update Atividade8.php

--- Atividade8.php
+++ Atividade8.php
@@ -3,31 +3,28 @@
 print "Digite o valor de três produtos e depois veja qual é o mais barato entre os três : \n\n"  ;
 
     print "Digite o valor do primeiro produto: ";
-    $produto1 = (int) fgets (STDIN);
+    $p1 = (int) fgets (STDIN);
     
     print "Digite o valor do segundo produto: ";
-    $produto2 = (int) fgets (STDIN);
+    $p2 = (int) fgets (STDIN);
 
     print "Digite o valor do terceiro produto: ";
-    $produto3 = (int) fgets (STDIN);
+    $p3 = (int) fgets (STDIN);
 
 
 print "Agora veja quais dos produtos possui o melhor preço  (lembrando que o melhor preço é o do produto que for mais barato) :\n\n" ;
 
-if ($produto1 < $produto2 or $produto1 < $produto3) {
-    print "Então o $produto1 possui o menor preço dentre os três " ;
+if ($p1 < $p2 or $p1 < $p3) {
+    print " Então o produto 1 é o mais barato  " ;
 }
 
-if ($produto2 < $produto1 or $produto2 < $produto3) {
-    print "Então o $produto2 possui o menor preço dentre os três " ;
+if ($p2 < $p1 or $p2 < $p3) {
+    print "Então o produto 2 é o mais barato  " ;
 }
 
-if ($produto3 < $produto1 or $produto3 < $produto2) {
-    print "Então o $produto3 possui o menor preço dentre os três " ;
+if ($p3 < $p1 or $p3 < $p2) {
+    print "Então o produto 3 é o mais barato  " ;
 }
 
-
-
-print "O produto que possui  o valor mais barato, é o melhor para se comprar . " ;
 
 print "Fim do Algoritmo" ;
