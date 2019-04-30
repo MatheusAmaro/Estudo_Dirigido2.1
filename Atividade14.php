9d9ed4320aba3ee6efb3169ad48d27af3fb2a1a5
Matheus Amaro Maciel <48133433+MatheusAmaro@users.noreply.github.com>
2019-04-30 20:24:48 -0300

    Update Atividade14.php

--- Atividade14.php
+++ Atividade14.php
@@ -2,14 +2,16 @@
 
 print " Mostre-me os números de 1 há 50 e mostre somente os números impares : /n/n" ; 
 
-$num = 1 ;
 
-while ( $num < 50  ) {
+$contador = 1; 
 
- print= " $num ";
 
- $num= $num + 1  ;
+while ( $contador < 50  ) {
+
+ print= " $contador ";
+ $contador= $contador  + 1 ;
 }
+
  if ($contador % 2 == 0){
      echo "par";
 } else {
@@ -23,13 +25,6 @@ while ( $num < 50  ) {
 print " Fim do Algoritmo ";
 
 #$contador = 0 ;
-
-while ( $contador > 1  ) {
-
- print= " $contador ";
-
- $contador= $contador -1 ;
-}#
 
 
 
